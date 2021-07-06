fb09b7b42f2f1813263d0506c499c8206ebeb8fd
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-05 23:12:04 -0400

    add svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
+};
