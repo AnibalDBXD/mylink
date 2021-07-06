62f25ecacd2b6e49826752f70338d3be35c56a7a
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-05 23:13:11 -0400

    Add toggleColor in all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import ToggleColor from "../components/ToggleColor";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ChakraProvider>
+      <ToggleColor />
       <Component {...pageProps} />
     </ChakraProvider>
   );
